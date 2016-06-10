a8eff776d220e63f5231a96957a8fc27bebd4d70
Saravanan Rathinakumar <srathinakumar@visolve.com>
2016-06-10 09:57:30 +0530

    valueset table primary key null issue fix

--- sql/4_2_2-to-4_3_1_upgrade.sql
+++ sql/4_2_2-to-4_3_1_upgrade.sql
@@ -74,11 +74,11 @@
 
 #IfNotTable valueset
 CREATE TABLE IF NOT EXISTS `valueset` (
-  `nqf_code` varchar(255) DEFAULT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `code_system` varchar(255) DEFAULT NULL,
+  `nqf_code` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `code_system` varchar(255) NOT NULL,
   `code_type` varchar(255) DEFAULT NULL,
-  `valueset` varchar(255) DEFAULT NULL,
+  `valueset` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `valueset_name` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`nqf_code`,`code`,`valueset`)
